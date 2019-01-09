04ee52428b2c7a38f2a06514effe0c4ff2bd1309
Mike <mike@e-cube.nl>
2019-01-09 22:27:33 +0100

    add perception distance

--- js/app.js
+++ js/app.js
@@ -10,19 +10,55 @@ const encounterDistances = {
     'swamp': '2d8*10',
     'mountains': '4d10*10',
     'plains': '6d6*40',
-    'urban': '2d20*5',
+    'urban': '1d12*5',
     'cwater': '4d8*10',
     'mwater': '1d8*10',
 };
+
+const perceptionContainer = document.getElementById('perceptionMenuContainer');
+const encounterContainer = document.getElementById('encounterTilesContainer');
 const distanceTextField = document.getElementById('distanceBox');
+const maxDistanceField = document.getElementById('maxDistance');
 
 function setup() {
     document.querySelectorAll('.encounter-tile').forEach(function (tile) {
         tile.addEventListener('click', getTerrainDistance);
     });
+
+    toggleTerrainTiles();
+    document.getElementById('encounterCollapse').addEventListener('click', toggleTerrainTiles);
+    document.getElementById('perceptionDistanceContainer').addEventListener('click', toggleHamburger);
+    document.getElementById('perceptionInput').addEventListener('keyup', determineMaxSpotDistance);
 }
 
 function getTerrainDistance(event) {
     let encounterDistance = roll(encounterDistances[this.id]);
-    distanceTextField.textContent = `${encounterDistance}ft/${encounterDistance/5}"`;
+    distanceTextField.textContent = `${encounterDistance}ft/${encounterDistance / 5}"`;
+}
+
+function toggleTerrainTiles() {
+    console.log(encounterContainer.style.display);
+    if (encounterContainer.style.maxHeight) {
+        encounterContainer.style.maxHeight = null;
+    }
+    else {
+        encounterContainer.style.maxHeight = encounterContainer.scrollHeight + "px";
+    }
+}
+
+function toggleHamburger() {
+    this.classList.toggle('change');
+    console.log(perceptionContainer.style.transform);
+    if (this.classList.contains('change')) {
+        perceptionContainer.style.transform = 'translate(0,0)';
+    }
+    else {
+        perceptionContainer.style.transform = 'translate(-300px, 0px)';
+    }
+}
+
+function determineMaxSpotDistance (event) {
+    console.log(this.value);
+    let perception = this.value;
+    maxDistanceField.textContent = `${perception*10}ft/${perception*5}"`
 }
